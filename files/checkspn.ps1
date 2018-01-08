fee8d798b8cd7c5998966f52cd446436772209c5
matt6697 <32440697+matt6697@users.noreply.github.com>
2018-01-08 08:02:18 +0100

    Removed deprecated files used to register SPN (now done with xActiveDirectory)

--- files/checkspn.ps1
+++
@@ -1,9 +0,0 @@
-﻿Param(
-    [string]$spn,
-    [string]$serviceaccount
-)
-
-if( (setspn -l $serviceaccount | Select-String $spn).count -gt 0) {
-    echo 'SPN already registered for the target service account'
-    exit 1
-}
