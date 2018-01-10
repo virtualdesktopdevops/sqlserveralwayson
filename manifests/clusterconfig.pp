abd8090b42caea5565987ca6a46030d07c9973b8
matt6697 <32440697+matt6697@users.noreply.github.com>
2018-01-10 08:16:28 +0100

    Paging (indent) fixes

--- manifests/clusterconfig.pp
+++ manifests/clusterconfig.pp
@@ -1,35 +1,35 @@
 class sqlserveralwayson::clusterconfig inherits sqlserveralwayson {
- 
+
   if ( $role == 'primary' ) {
-		#Failover cluster creation
-		dsc_xcluster{'CreateFailoverCluster':
-			dsc_name => $clusterName,
-			dsc_staticipaddress => $clusterIP,
-			dsc_domainadministratorcredential => {'user' => $setup_svc_username, 'password' => $setup_svc_password}
-		}
-		
-		#File share whitness configuration
-		#Warning, bug https://github.com/PowerShell/xFailOverCluster/issues/35 on Windows 2016
-		dsc_xclusterquorum{'SetQuorumToNodeAndDiskMajority':
-			dsc_issingleinstance => 'Yes',
-			dsc_type => 'NodeAndFileShareMajority',
-			dsc_resource => $fileShareWitness,
-			require => Dsc_xcluster['CreateFailoverCluster']
-     }
-		
-	}
-	else {
-	  dsc_xwaitforcluster{'SecondaryReplicaWaitForCluster':
-	    dsc_name => $clusterName,
-	    dsc_retryintervalsec => 10,
+    #Failover cluster creation
+    dsc_xcluster{'CreateFailoverCluster':
+      dsc_name => $clusterName,
+      dsc_staticipaddress => $clusterIP,
+      dsc_domainadministratorcredential => {'user' => $setup_svc_username, 'password' => $setup_svc_password}
+    }
+
+    #File share whitness configuration
+    #Warning, bug https://github.com/PowerShell/xFailOverCluster/issues/35 on Windows 2016
+    dsc_xclusterquorum{'SetQuorumToNodeAndDiskMajority':
+      dsc_issingleinstance => 'Yes',
+      dsc_type => 'NodeAndFileShareMajority',
+      dsc_resource => $fileShareWitness,
+      require => Dsc_xcluster['CreateFailoverCluster']
+    }
+
+  }
+  else {
+    dsc_xwaitforcluster{'SecondaryReplicaWaitForCluster':
+      dsc_name => $clusterName,
+      dsc_retryintervalsec => 10,
       dsc_retrycount => 6
-	  }
-	  
-	  dsc_xcluster{'JoinCluster':
-	    dsc_name => $clusterName,
+    }
+
+    dsc_xcluster{'JoinCluster':
+      dsc_name => $clusterName,
       dsc_staticipaddress => $clusterIP,
       dsc_domainadministratorcredential => {'user' => $setup_svc_username, 'password' => $setup_svc_password},
       require => Dsc_xwaitforcluster['SecondaryReplicaWaitForCluster']
     }
-	}
+  }
 }
--- manifests/serviceaccounts.pp
+++ manifests/serviceaccounts.pp
@@ -1,10 +1,10 @@
 class sqlserveralwayson::serviceaccounts inherits sqlserveralwayson {
-  #Needed for ActiveDirectory remote management using Powershell
+	#Needed for ActiveDirectory remote management using Powershell
 	dsc_windowsfeature{ 'RSAT-AD-Powershell':
-	 dsc_ensure => 'Present',
-	 dsc_name => 'RSAT-AD-Powershell'
+		dsc_ensure => 'Present',
+		dsc_name => 'RSAT-AD-Powershell'
 	}
-	
+
 	#SQL service account creation (Active Directory)
 	dsc_xaduser{'SvcSQLAccount':
 		dsc_domainname => $domainName,
@@ -14,24 +14,24 @@ class sqlserveralwayson::serviceaccounts inherits sqlserveralwayson {
 		dsc_ensure => 'Present',
 		require => Dsc_windowsfeature['RSAT-AD-Powershell']
 	}
-	
+
 	#Configure MSSQLSvc SPN on SQL service account
-  dsc_xadserviceprincipalname{'SvcSQLSPN':
-    dsc_account => $sqlservicecredential_username,
-    dsc_serviceprincipalname => "MSSQLSvc/${fqdn}",
-    dsc_ensure => present,
-    dsc_psdscrunascredential => {'user' => $setup_svc_username, 'password' => $setup_svc_password},
-    require => Dsc_xaduser['SvcSQLAccount']
-  }
-	
+	dsc_xadserviceprincipalname{'SvcSQLSPN':
+		dsc_account => $sqlservicecredential_username,
+		dsc_serviceprincipalname => "MSSQLSvc/${fqdn}",
+		dsc_ensure => present,
+		dsc_psdscrunascredential => {'user' => $setup_svc_username, 'password' => $setup_svc_password},
+		require => Dsc_xaduser['SvcSQLAccount']
+	}
+
 	#SQL Agent service account creation (Active Directory)
-  dsc_xaduser{'SvcSQLAgentAccount':
-    dsc_domainname => $domainName,
-    dsc_domainadministratorcredential => {'user' => $setup_svc_username, 'password' => $setup_svc_password},
-    dsc_username => $sqlagentservicecredential_username,
-    dsc_password => {'user' => $sqlagentservicecredential_username, 'password' => $sqlagentservicecredential_password},
-    dsc_ensure => 'Present',
-    require => Dsc_windowsfeature['RSAT-AD-Powershell']
-  }
-	  
+	dsc_xaduser{'SvcSQLAgentAccount':
+		dsc_domainname => $domainName,
+		dsc_domainadministratorcredential => {'user' => $setup_svc_username, 'password' => $setup_svc_password},
+		dsc_username => $sqlagentservicecredential_username,
+		dsc_password => {'user' => $sqlagentservicecredential_username, 'password' => $sqlagentservicecredential_password},
+		dsc_ensure => 'Present',
+		require => Dsc_windowsfeature['RSAT-AD-Powershell']
+	}
+
 }
