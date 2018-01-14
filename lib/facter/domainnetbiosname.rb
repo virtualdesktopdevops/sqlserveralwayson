b10f3573c34c7f4869d82b31ecae8caff6456850
matt6697 <32440697+matt6697@users.noreply.github.com>
2018-01-14 20:06:57 +0100

    Bugfix : WMI query returning multiple objects. Empty netbiosdomainname fact.

--- lib/facter/domainnetbiosname.rb
+++ lib/facter/domainnetbiosname.rb
@@ -4,8 +4,9 @@ Facter.add('domainnetbiosname') do
   setcode do
     begin
       require 'win32ole'
+	dnsforestname = Facter.value(:domain)
       wmi = WIN32OLE.connect("winmgmts:\\\\.\\root\\cimv2")
-      win32ntdomain = wmi.ExecQuery("SELECT * FROM Win32_NTDomain").each.first
+      win32ntdomain = wmi.ExecQuery("SELECT * FROM Win32_NTDomain WHERE DnsForestName='#{dnsforestname}'").each.first
       win32ntdomain.DomainName
     rescue
       nil
